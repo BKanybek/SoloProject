b96790123bb47c95135d5f1a1bef2596b907ce3c
BKanybek <95636162+BKanybek@users.noreply.github.com>
2022-02-28 21:05:44 +0400

    6

--- src/Components/Admin/EditObject/EditObject.jsx
+++ src/Components/Admin/EditObject/EditObject.jsx
@@ -1,189 +1,117 @@
-import React, { useEffect, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { useNavigate, useParams } from 'react-router-dom';
-import { clearUserToEdit, getUserToEdit, saveEditedUser } from '../../Actions/UserActions';
-import { Formik } from 'formik';
-import * as yup from 'yup';
-import { toast } from 'react-toastify';
-import { Button, Form } from 'react-bootstrap';
+import React, { useState } from 'react';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import { Button, TextField } from '@mui/material';
+import{ Link, useParams } from 'react-router-dom';
+import { productContext } from '../../../Contexts/ProductsContext'
 
-const EditPage = () => {
-    const dispatch = useDispatch() 
-    const navigate = useNavigate()
-    const params = useParams()
-    const userToEdit = useSelector((state) => state.userReducer.userToEdit)
-    const [editUser, setEditUser] = useState(userToEdit)
+export default function EditObject  () {
+    const [values, setValues] = useState({
+        title: '',
+        image: '',
+        price: '',
+        type: '',
+        description: ''
+    })
 
-    useEffect(() => {
-        dispatch(getUserToEdit(params.id))
-    }, [])
+    const { edit, editProduct, saveEditedProduct } = React.useContext(productContext)
 
-    useEffect(() => {
-        setEditUser(userToEdit)
-    }, [userToEdit])
+    const {id} = useParams()
 
-    const schema = yup.object().shape({
-        first_name: yup
-          .string()
-          .min(2, "Too short")
-          .max(256, "Too long")
-          .required("Required"),
-        last_name: yup
-          .string()
-          .min(2, "Too short")
-          .max(256, "Too long")
-          .required("Required"),
-        birth_date: yup.string().required("Required"),
-        gender: yup.string().required("Required"),
-        job: yup
-          .string()
-          .min(2, "Too short")
-          .max(256, "Too long")
-          .required("Required"),
-        biography: yup
-          .string()
-          .min(2, "Too short")
-          .max(1024, "Too long")
-          .required("Required"),
-        is_active: yup.bool().required("Required"),
-      });
+    React.useEffect(()=>{
+        editProduct(id)
+    }, [id])
 
-    const handleClick = (data) => {
-        dispatch(saveEditedUser(data, toast))
-        dispatch(clearUserToEdit())
-        navigate('/')
+    React.useEffect(() => {
+        if(edit) {
+            setValues(edit)
+        }
+    }, [edit])
+
+    const handleEditInp = (e) => {
+        let obj ={
+            ...values,
+            [e.target.name]: e.target.value
+        }
+        setValues(obj)
     }
 
-    return (
-        <div className="container mt-5 mb-5">
-      <div className="mb-5">
-        {/* <Link to="/"> */}
-        <Button
-          onClick={() => {
-            dispatch(clearUserToEdit());
-            navigate('/')
-          }}
-          className="btn-success"
-        >
-          Main page
-        </Button>
-        {/* </Link> */}
-      </div>
-      {editUser ? (
-        <Formik
-          validationSchema={schema}
-          onSubmit={(data) => handleClick(data)}
-          initialValues={editUser}
-        >
-          {({ handleSubmit, handleChange, values, touched, errors }) => (
-            <Form onSubmit={handleSubmit}>
-              <Form.Group className="mb-3">
-                <Form.Label>First name</Form.Label>
-                <Form.Control
-                  onChange={handleChange}
-                  name="first_name"
-                  type="text"
-                  placeholder="First name"
-                  value={values.first_name}
-                  isValid={touched.first_name && !errors.first_name}
-                  isInvalid={!!errors.first_name}
-                />
-                <Form.Control.Feedback type="invalid">
-                  {errors.last_name}
-                </Form.Control.Feedback>
-              </Form.Group>
-              <Form.Group className="mb-3">
-                <Form.Label>Last name</Form.Label>
-                <Form.Control
-                  onChange={handleChange}
-                  name="last_name"
-                  type="text"
-                  placeholder="Last name"
-                  value={values.last_name}
-                  isValid={touched.last_name && !errors.last_name}
-                  isInvalid={!!errors.last_name}
-                />
-                <Form.Control.Feedback type="invalid">
-                  {errors.last_name}
-                </Form.Control.Feedback>
-              </Form.Group>
-              <Form.Group className="mb-3">
-                <Form.Label>DOB</Form.Label>
-                <Form.Control
-                  onChange={handleChange}
-                  name="birth_date"
-                  type="date"
-                  placeholder="Date of birthday"
-                  value={values.birth_date}
-                  isValid={touched.birth_date && !errors.birth_date}
-                  isInvalid={!!errors.birth_date}
-                />
-                <Form.Control.Feedback type="invalid">
-                  {errors.birth_date}
-                </Form.Control.Feedback>
-              </Form.Group>
-              <Form.Group className="mb-3">
-                <Form.Label>Sex</Form.Label>
-                <Form.Select
-                  onChange={handleChange}
-                  name="gender"
-                  className="me-sm-2"
-                  value={values.gender}
-                  isValid={touched.gender && !errors.gender}
-                  isInvalid={!!errors.gender}
-                >
-                  <option value="">Choose...</option>
-                  <option value="male">male</option>
-                  <option value="female">female</option>
-                </Form.Select>
-              </Form.Group>
-              <Form.Group className="mb-3">
-                <Form.Label>Job</Form.Label>
-                <Form.Control
-                  onChange={handleChange}
-                  name="job"
-                  type="text"
-                  placeholder="Job"
-                  value={values.job}
-                  isValid={touched.job && !errors.job}
-                  isInvalid={!!errors.job}
-                />
-                <Form.Control.Feedback type="invalid">
-                  {errors.job}
-                </Form.Control.Feedback>
-              </Form.Group>
-              <Form.Group className="mb-3">
-                <Form.Label>Biography</Form.Label>
-                <Form.Control
-                  onChange={handleChange}
-                  name="biography"
-                  as="textarea"
-                  rows={3}
-                  value={values.biography}
-                  isValid={touched.biography && !errors.biography}
-                  isInvalid={!!errors.biography}
-                  
-                />
-                <Form.Control.Feedback type="invalid">
-                  {errors.biography}
-                </Form.Control.Feedback>
-              </Form.Group>
-              <Form.Group className="mb-3">
-                <Form.Check
-                  checked={values.is_active}
-                  onChange={handleChange}
-                  name="is_active"
-                  type="checkbox"
-                  label="Is active"
-                />
-              </Form.Group>
-              <Button type="submit">Save changes</Button>
-            </Form>
-          )}
-        </Formik>
-      ) : null}
-    </div>
-    );
-};
+    const handleSave = () => {
+        saveEditedProduct(values)
+    }
 
-export default EditPage;
+
+
+    return (
+        <Box
+      sx={{
+        display: 'flex',
+        flexWrap: 'wrap',
+        '& > :not(style)': {
+          m:'40px auto',
+          maxWidth: 1000,
+          height: 'auto',
+          padding: '10px'
+        },
+      }}
+    >
+        <Paper elevation={3}>
+            <h1>Изменить данные</h1>
+                <div style={{
+                    display: 'flex',
+                    justifyContent:'space-around',
+                    color: 'black'
+                }}>
+                    <div>
+                        <img width='300' src={values.image} alt={values.title} />
+                    </div>
+                    <div style={{
+                         width: '450 px',
+                         display: 'flex',
+                         alignItems:'center',
+                         flexDirection: 'column',
+                         justifyContent: 'center'
+                    }}>
+                        <form noValidate
+                        autoComplete='off' style={{
+                            display: 'flex', 
+                            flexDirection: 'column',
+                            justifyContent: 'center',
+                            alignItems: 'center'
+                        }}>
+                        <TextField 
+                        style={{padding: '10px'}} name='title' 
+                        onChange={handleEditInp} 
+                        value={values.title} variant='outlined' 
+                        label='Title'/>
+                    <TextField 
+                        style={{padding: '10px'}} name='image' 
+                        onChange={handleEditInp} 
+                        value={values.image} variant='outlined' 
+                        label='Image'/>
+                    <TextField 
+                        style={{padding: '10px'}} name='price' 
+                        onChange={handleEditInp} 
+                        value={values.price} variant='outlined' 
+                        label='Price'/>
+                    <TextField 
+                        style={{padding: '10px'}} name='type' 
+                        onChange={handleEditInp} 
+                        value={values.type} variant='outlined' 
+                        label='Type'/>
+                    <TextField 
+                        style={{padding: '10px'}} name='description' 
+                        onChange={handleEditInp} 
+                        value={values.description} variant='outlined' 
+                        label='Description'/>
+                </form>
+                <Link to='/'>
+                    <Button onClick={handleSave} variant='contained' color='warning'>Save</Button>
+                </Link>
+                    </div>
+            </div>
+      </Paper>
+    </Box>
+  );
+}
+
--- src/Components/Admin/ListObject/ListObject.jsx
+++ src/Components/Admin/ListObject/ListObject.jsx
@@ -1,60 +1,140 @@
-import React, {useEffect} from 'react';
-import { Table, Button } from 'react-bootstrap';
-import {useDispatch, useSelector } from 'react-redux';
-import {Link, useNavigate } from 'react-router-dom';
-import { ToastContainer } from 'react-toastify';
-import { getUsers } from '../../Actions/UserActions';
+import { getOptionGroupUnstyledUtilityClass } from '@mui/base';
+import { Box, Grid, Pagination, Stack } from '@mui/material';
+import React, { useContext, useEffect, useState } from 'react';
+import { useNavigate, useSearchParams} from 'react-router-dom';
+import ProductCard from '../ProductCard/ProductCard';
+import SideBar from '../../Home/SideBar/SideBar'
+import SearchIcon from '@mui/icons-material/Search';
+import { styled, alpha } from '@mui/material/styles';
+import InputBase from '@mui/material/InputBase';
+import { productContext } from '../../../Contexts/ProductsContext';
 
-const ListPage = () => {
-    const dispatch = useDispatch()
-    const users = useSelector((state) => state.userReducer.users)
+
+
+const Search = styled('div')(({ theme }) => ({
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: alpha(theme.palette.common.white, 0.15),
+    '&:hover': {
+      backgroundColor: alpha(theme.palette.common.white, 0.25),
+    },
+    marginRight: theme.spacing(2),
+    marginLeft: 0,
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      marginLeft: theme.spacing(3),
+      width: 'auto',
+    },
+  }));
+  
+  const SearchIconWrapper = styled('div')(({ theme }) => ({
+    padding: theme.spacing(0, 2),
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  }));
+  
+  const StyledInputBase = styled(InputBase)(({ theme }) => ({
+    color: 'inherit',
+    '& .MuiInputBase-input': {
+      padding: theme.spacing(1, 1, 1, 0),
+      // vertical padding + font size from searchIcon
+      paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+      transition: theme.transitions.create('width'),
+      width: '100%',
+      [theme.breakpoints.up('md')]: {
+        width: '20ch',
+      },
+    },
+  }));
+
+const ListObject = () => {
+    const { products, getProducts, paginatedPages } = useContext(productContext)
+    const search = new URLSearchParams(window.location.search)
+    const [searchParams, setSearchParams] = useSearchParams()
+    const [limit, setLimit] = useState(10)
+    const[ searchVal, setSearchVal ] = React.useState(searchParams.get('q') ? searchParams.get('q') : '')
+    const [page, setPage] = useState(searchParams.get('_page') ? searchParams.get("_page") : 1)
     const navigate = useNavigate()
 
+   
+
+    useEffect(() =>{
+        setSearchParams({
+            '_limit': limit,
+            "_page": page
+        })
+    }, [limit, page])
+
     useEffect(() => {
-        dispatch(getUsers())
+        getProducts()
     }, [])
 
+    const handleValue = (e) => {
+        const search = new URLSearchParams(window.location.search)
+        search.set('q', e.target.value)
+        setSearchVal(e.target.value)
+        setSearchParams({
+            'q': searchVal,
+            '_limit': 3,
+            '_page': 1
+        })
+        getProducts()
+    }
 
-    const pushToDetail = (id) => {
-        navigate(`/detail/${id}`)
+
+
+    const handlePage = (e, pageVal) => {
+        let newPath = `${window.location.pathname}?${search.toString()}`
+        navigate(newPath)
+        setSearchParams({'_page': pageVal, '_limit': limit})
+        getProducts()
+        setPage(pageVal)
     }
 
     return (
-        <div className="container mt-5">
-      <div className="mb-5">
-        <Link to="/create">
-          <Button>Add new user</Button>
-        </Link>
-      </div>
-      <Table striped bordered hover size="xl">
-        <thead>
-          <tr>
-            <th>#</th>
-            <th>First Name</th>
-            <th>Last Name</th>
-            <th>DOB</th>
-            <th>Sex</th>
-          </tr>
-        </thead>
-        <tbody>
-          {users.map((item, index) => (
-            <tr
-              key={item.id}
-              onClick={() => pushToDetail(item.id)}
-              style={{ cursor: "pointer" }}
-            >
-              <td>{index + 1}</td>
-              <td>{item.first_name}</td>
-              <td>{item.last_name}</td>
-              <td>{item.birth_date}</td>
-              <td>{item.gender}</td>
-            </tr>
-          ))}
-        </tbody>
-      </Table>
-      <ToastContainer />
-    </div>
+        <div style={{}}>
+            <div style={{backgroundColor: 'black', width: '100%',height:' 100px'}}></div>
+            <SideBar />  
+            <Search  >
+              <SearchIconWrapper  >
+                <SearchIcon  />
+              </SearchIconWrapper>
+              <StyledInputBase
+                placeholder="Search…"
+                inputProps={{ 'aria-label': 'search' }}
+                value={searchVal}
+                onChange={handleValue}
+                sx={{border: '2px solid'}} 
+              />
+          </Search>
+
+            <Box sx={{flexGrow: 1, margin: 4}}>
+                <Grid container spacing={{xs: 1, md: 6}} columns={{xs: 1, sm: 3, md: 12}}>
+                    {
+                        products ? (
+                            products.map((item, index) => (
+                                    <Grid sx={{display: 'flex', justifyContent: 'center'}} item xs={1} sm={4} md={4}  key={index}>
+                                        <ProductCard item={item} key={index}/>
+                                    </Grid>
+                            ))
+                        ): (<h1>Loading...</h1>)
+                    }
+                </Grid>
+                <Stack spacing={2}>
+                    <Pagination 
+                        count={paginatedPages}
+                        onChange={handlePage}
+                        page={+page}
+                        sx={{display: 'flex', justifyContent: 'center', padding: 4}}
+                    />
+                </Stack>
+            </Box>
+        </div>
     );
 };
 
-export default ListPage;
+export default ListObject;
